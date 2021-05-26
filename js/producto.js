675f76a406f3816d2baba355b6d04be0ca5fca85
darreceygor <66496611+darreceygor@users.noreply.github.com>
2021-05-26 13:50:34 -0300

    Add files via upload

---
+++ js/producto.js
@@ -0,0 +1,32 @@
+
+let contenido = document.querySelector('#contenido') /*guarda el id en la variable*/
+
+
+function traer(){
+    fetch('json/tabla.json') /*toma la direccion del arhivo json a tratar*/
+    .then(res => res.json()) /*traemos el archivo y lo transformamos a objeto json*/
+    .then(datos =>{   /*traemos los datos para procesar*/
+        tabla(datos) /*llamamos a la fc tabla que pondra los datos en la pagina */
+    })
+}
+
+function tabla(datos){
+    contenido.innerHTML='' /*borra datos existentes*/
+ 
+    for (let valor of datos) {         /*pongo el valor de cada campo*/
+        
+            contenido.innerHTML +=`
+                <tr>
+                    <td>${valor.id}</td>
+                    <td>${valor.fecha}</td>
+                    <td>${valor.componente}</td>
+                    <td>${valor.cantidad}</td>
+                    <td>${valor.modelo}</td>
+                    <td>${valor.descripcion}</td>
+                </tr>
+        `
+    }
+}
+
+/*La API Fetch es una funcionalidad con la cual podemos realizar 
+peticiones asíncronas y obtener respuesta a las mismas de una forma sencilla.*/
