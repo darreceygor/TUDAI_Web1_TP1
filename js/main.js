5b9a34b688c11ffde259c339385021cdf8dd9de1
darreceygor <66496611+darreceygor@users.noreply.github.com>
2021-05-28 16:12:32 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ $(document).ready(function() {
         $('ul').toggleClass('show');
     });
 });
+
+
+window.onload = function(){
+
+
+//Se cargan los productos dentro del HTML de forna dinamica haciendo uso de los datos de la base de datos, como si de un PHP se tratase:
+var DIVS = document.getElementsByName("DIVS");
+for (i in productos){
+    DIVS[i].innerHTML = 
+            `
+        <div class="item">    
+                <img id="img-item" src="${imgPeque[i]}">
+            <p>   
+                <h4>${productos[i]}</h4><br>
+                <td>${precios[i]}</td><br>
+                
+            </p>
+  
+        </div>
+        `
+}
