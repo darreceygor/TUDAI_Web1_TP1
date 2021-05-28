0738179cf125dfb724aae93c6a06d4afd0e9ec59
darreceygor <66496611+darreceygor@users.noreply.github.com>
2021-05-28 16:10:10 -0300

    Update producto.js

--- js/producto.js
+++ js/producto.js
@@ -2,7 +2,7 @@
 let contenido = document.querySelector('#contenido') /*guarda el id en la variable*/
 
 
-function traer(){
+function traerTabla(){
     fetch('json/tabla.json') /*toma la direccion del arhivo json a tratar*/
     .then(res => res.json()) /*traemos el archivo y lo transformamos a objeto json*/
     .then(datos =>{   /*traemos los datos para procesar*/
@@ -30,3 +30,29 @@ function tabla(datos){
 
 /*La API Fetch es una funcionalidad con la cual podemos realizar 
 peticiones asíncronas y obtener respuesta a las mismas de una forma sencilla.*/
+
+
+function traerProducto(){
+    fetch('json/items.json') /*toma la direccion del arhivo json a tratar*/
+    .then(res => res.json()) /*traemos el archivo y lo transformamos a objeto json*/
+    .then(datosProd =>{   /*traemos los datos para procesar*/
+        productos(datosProd) /*llamamos a la fc tabla que pondra los datos en la pagina */
+    })
+}
+
+function productos(datosProd){
+    
+let DIVS = document.getElementsByName("DIVS");
+    
+for (i in productos){
+    DIVS[i].innerHTML = 
+            `
+            <div class="item">    
+                <img id="img-item" src="${imgPeque[i]}">
+                <p>   
+                    <h4>${productos[i]}</h4><br>
+                    <td>${precios[i]}</td><br>
+                </p>
+           </div>
+        `
+}
